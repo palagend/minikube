aaa0fc3e59cbf1de773b2435b77d113cf7577034
Grain Stark <palagend@gmail.com>
2018-12-17 19:46:33 +0800

    Replace k8s.gcr.io with registry.cn-hangzhou.aliyuncs.com/google_containers

--- pkg/minikube/constants/constants.go
+++ pkg/minikube/constants/constants.go
@@ -209,10 +209,10 @@ const FileScheme = "file"
 func GetKubeadmCachedImages(kubernetesVersionStr string) []string {
 
 	var images = []string{
-		"k8s.gcr.io/kube-proxy-amd64:" + kubernetesVersionStr,
-		"k8s.gcr.io/kube-scheduler-amd64:" + kubernetesVersionStr,
-		"k8s.gcr.io/kube-controller-manager-amd64:" + kubernetesVersionStr,
-		"k8s.gcr.io/kube-apiserver-amd64:" + kubernetesVersionStr,
+		"registry.cn-hangzhou.aliyuncs.com/google_containers/kube-proxy-amd64:" + kubernetesVersionStr,
+		"registry.cn-hangzhou.aliyuncs.com/google_containers/kube-scheduler-amd64:" + kubernetesVersionStr,
+		"registry.cn-hangzhou.aliyuncs.com/google_containers/kube-controller-manager-amd64:" + kubernetesVersionStr,
+		"registry.cn-hangzhou.aliyuncs.com/google_containers/kube-apiserver-amd64:" + kubernetesVersionStr,
 	}
 
 	gt_v1_10 := semver.MustParseRange(">=1.11.0")
@@ -227,35 +227,35 @@ func GetKubeadmCachedImages(kubernetesVersionStr string) []string {
 
 	if v1_10(kubernetesVersion) || gt_v1_10(kubernetesVersion) {
 		images = append(images, []string{
-			"k8s.gcr.io/pause-amd64:3.1",
-			"k8s.gcr.io/k8s-dns-kube-dns-amd64:1.14.8",
-			"k8s.gcr.io/k8s-dns-dnsmasq-nanny-amd64:1.14.8",
-			"k8s.gcr.io/k8s-dns-sidecar-amd64:1.14.8",
-			"k8s.gcr.io/etcd-amd64:3.1.12",
+			"registry.cn-hangzhou.aliyuncs.com/google_containers/pause-amd64:3.1",
+			"registry.cn-hangzhou.aliyuncs.com/google_containers/k8s-dns-kube-dns-amd64:1.14.8",
+			"registry.cn-hangzhou.aliyuncs.com/google_containers/k8s-dns-dnsmasq-nanny-amd64:1.14.8",
+			"registry.cn-hangzhou.aliyuncs.com/google_containers/k8s-dns-sidecar-amd64:1.14.8",
+			"registry.cn-hangzhou.aliyuncs.com/google_containers/etcd-amd64:3.1.12",
 		}...)
 
 	} else if v1_9(kubernetesVersion) {
 		images = append(images, []string{
-			"k8s.gcr.io/pause-amd64:3.0",
-			"k8s.gcr.io/k8s-dns-kube-dns-amd64:1.14.7",
-			"k8s.gcr.io/k8s-dns-dnsmasq-nanny-amd64:1.14.7",
-			"k8s.gcr.io/k8s-dns-sidecar-amd64:1.14.7",
-			"k8s.gcr.io/etcd-amd64:3.1.10",
+			"registry.cn-hangzhou.aliyuncs.com/google_containers/pause-amd64:3.0",
+			"registry.cn-hangzhou.aliyuncs.com/google_containers/k8s-dns-kube-dns-amd64:1.14.7",
+			"registry.cn-hangzhou.aliyuncs.com/google_containers/k8s-dns-dnsmasq-nanny-amd64:1.14.7",
+			"registry.cn-hangzhou.aliyuncs.com/google_containers/k8s-dns-sidecar-amd64:1.14.7",
+			"registry.cn-hangzhou.aliyuncs.com/google_containers/etcd-amd64:3.1.10",
 		}...)
 
 	} else if v1_8(kubernetesVersion) {
 		images = append(images, []string{
-			"k8s.gcr.io/pause-amd64:3.0",
-			"k8s.gcr.io/k8s-dns-kube-dns-amd64:1.14.5",
-			"k8s.gcr.io/k8s-dns-dnsmasq-nanny-amd64:1.14.5",
-			"k8s.gcr.io/k8s-dns-sidecar-amd64:1.14.5",
-			"k8s.gcr.io/etcd-amd64:3.0.17",
+			"registry.cn-hangzhou.aliyuncs.com/google_containers/pause-amd64:3.0",
+			"registry.cn-hangzhou.aliyuncs.com/google_containers/k8s-dns-kube-dns-amd64:1.14.5",
+			"registry.cn-hangzhou.aliyuncs.com/google_containers/k8s-dns-dnsmasq-nanny-amd64:1.14.5",
+			"registry.cn-hangzhou.aliyuncs.com/google_containers/k8s-dns-sidecar-amd64:1.14.5",
+			"registry.cn-hangzhou.aliyuncs.com/google_containers/etcd-amd64:3.0.17",
 		}...)
 	}
 
 	images = append(images, []string{
-		"k8s.gcr.io/kubernetes-dashboard-amd64:v1.10.0",
-		"k8s.gcr.io/kube-addon-manager:v8.6",
+		"registry.cn-hangzhou.aliyuncs.com/google_containers/kubernetes-dashboard-amd64:v1.10.0",
+		"registry.cn-hangzhou.aliyuncs.com/google_containers/kube-addon-manager:v8.6",
 		"gcr.io/k8s-minikube/storage-provisioner:v1.8.1",
 	}...)
 
